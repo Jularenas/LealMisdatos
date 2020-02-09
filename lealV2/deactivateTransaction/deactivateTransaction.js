eb3efc6069e958a88ca4c47ab18d14d62a7385b9
Julian <jd.arenas@uniandes.edu.co>
2020-02-08 19:51:03 -0500

    transaction deactivate microservice done

---
+++ lealV2/deactivateTransaction/deactivateTransaction.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const crypto = require('crypto');
+const bodyParser = require('body-parser');
+
+const mysql = require('mysql');
+const port = process.argv.slice(2)[0];
+const app = express();
+app.use(bodyParser.json());
+
+var pool      =    mysql.createPool({
+    connectionLimit : 100, //important
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'misdatos',
+    debug    :  false
+});
+
+
+
+
+function deactivate_transaction_db(req,res) {
+   
+    pool.getConnection(function(err,connection){
+        if (err) {
+          res.json({"code" : 100, "status" : "Error in connection database"});
+          return;
+        }  
+
+        console.log('connected as id ' + connection.threadId);
+       
+
+        console.log(req.query);
+
+        connection.query(req.query,function(err,rows){
+            connection.release();
+            if(!err) {
+                res.json(rows);
+                return;
+            }          
+            else{
+                //console.log(err)
+                res.json(err);
+            }
+        });
+
+        connection.on('error', function(err) {      
+              res.json({"code" : 100, "status" : "Error in connection database"});
+              return;    
+        });
+  });
+}
+
+
+app.put('/inactivarTransaccion', (req,res)=>{
+    var trans_id=req.body.transaction_id;
+
+    var queryString="UPDATE transactions  SET status=0 WHERE transaction_id="+trans_id+";";
+    req.query=queryString;
+
+    deactivate_transaction_db(req,res);
+
+});
+
+console.log(`Transaction service listening on port ${port}`);
+app.listen(port);
